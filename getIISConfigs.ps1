fcfd0681410d656650cdb9e77599b7efb6603c3b
Kubiaczyk <michael.kubiaczyk@checkmarx.com>
2020-04-21 12:16:37 +0200

    Initial commit

---
+++ getIISConfigs.ps1
@@ -0,0 +1,22 @@
+﻿param ( 
+    [string]$target = $(throw "-target is required")
+)
+
+$webconfigs = @( "Checkmarx Access Control", "Checkmarx Results Service", "Checkmarx Web RestAPI\CxRestAPI\bin", "Checkmarx Web RestAPI\CxRestAPI", "Checkmarx Web Services\CxWebInterface", "CheckmarxWebPortal\Web\app", "CheckmarxWebPortal\Web" )
+
+foreach ( $path in $webconfigs ) {
+    if ( -not (Test-Path -Path $path) ) {
+        $OUT = mkdir $path
+    }
+    $file = "\\" + $target + "\d$\Program Files\Checkmarx\" + $path + "\web.config" 
+    $dest = $path + "\" + $target + "-web.config"
+    Copy-Item -Path $file -Destination $dest 
+}
+
+if ( -not (Test-Path -Path "Windows\System32\inetsrv\config") ) {
+    $OUT = mkdir "Windows\System32\inetsrv\config"    
+}
+$file = "\\" + $target + "\c$\Windows\System32\inetsrv\config\applicationHost.config"
+$dest = "Windows\System32\inetsrv\config\" + $target + "-applicationHost.config"
+Copy-Item -Path $file -Destination $dest
+
